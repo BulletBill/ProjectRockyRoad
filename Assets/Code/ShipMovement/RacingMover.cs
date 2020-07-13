68787146764958ce226530bf6db47dd5050f819e
Bill Caldwell <xiaolinslacker@gmail.com>
2020-07-13 07:23:35 -0500

    Started movement component

---
+++ Assets/Code/ShipMovement/RacingMover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacingMover : MonoBehaviour
+{
+    bool PauseMovement = false;
+
+	public float TurnSpeed;
+	public float TopSpeed;
+	public float Acceleration;
+
+	float Speed;
+    Quaternion MoveDirection;
+    float Friction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 MoveSpeed = new Vector3(0, Speed, 0);
+        transform.Translate(transform.up * (Speed * Time.deltaTime), Space.World);
+
+        if (PauseMovement) return;
+        transform.Rotate(new Vector3(0, 0, -TurnSpeed) * Time.deltaTime * Input.GetAxis("Horizontal"));
+        MoveDirection = transform.rotation;
+
+        float Accel = Input.GetAxis("Vertical");
+        Speed = Mathf.Clamp(Speed + Accel, -TopSpeed, TopSpeed);
+    }
+}
